70405853431079efb76fa4f76d1449d3e9587801
maarfelipe <matheus.maarfe@hotmail.com>
2023-05-05 20:03:14 -0300

    done hour_generator

--- ClockInGenerator/hour_generator.py
+++ ClockInGenerator/hour_generator.py
@@ -1,12 +1,30 @@
 import random
 from datetime import datetime, timedelta
 
-entry1 = (datetime.now().replace(hour=8, minute=1) + timedelta(minutes=random.randint(0, 39)))
-exit1 = (datetime.now().replace(hour=12, minute=1) + timedelta(minutes=random.randint(0, 14)))
-entry2 = (exit1 + timedelta(minutes=60)) + timedelta(minutes=random.randint(1, 14))
-exit2 = (entry2 + timedelta(minutes=(480 - int(((exit1 - entry1).total_seconds()) / 60))))
-
-print(f'Entry 1: {entry1.strftime("%H:%M")}\n',
-      f'Exit 1: {exit1.strftime("%H:%M")}\n',
-      f'Entry 2: {entry2.strftime("%H:%M")}\n',
-      f'Exit 2: {exit2.strftime("%H:%M")}')
+class Shift:
+    def __init__(self, start_time, end_time):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.duration = (end_time - start_time).total_seconds() / 60
+        
+class Workday:
+    def __init__(self):
+        self.shift1 = None
+        self.shift2 = None
+    
+    def generate_shifts(self):
+        start_time1 = datetime.now().replace(hour=8, minute=1) + timedelta(minutes=random.randint(0, 39))
+        end_time1 = datetime.now().replace(hour=12, minute=1) + timedelta(minutes=random.randint(0, 14))
+        start_time2 = end_time1 + timedelta(minutes=60) + timedelta(minutes=random.randint(1, 14))
+        end_time2 = start_time2 + timedelta(minutes=480 - int((end_time1 - start_time1).total_seconds() / 60))
+        
+        self.shift1 = Shift(start_time1, end_time1)
+        self.shift2 = Shift(start_time2, end_time2)
+            
+if __name__ == '__main__':
+      workday = Workday()
+      workday.generate_shifts()
+# print(workday.shift1.start_time.strftime("%H:%M"))
+# print(workday.shift1.end_time.strftime("%H:%M"))
+# print(workday.shift2.start_time.strftime("%H:%M"))
+# print(workday.shift2.end_time.strftime("%H:%M"))
