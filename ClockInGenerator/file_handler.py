8856ea90f6e9df82f064475b6d147eee4fbc61aa
maarfelipe <matheus.maarfe@hotmail.com>
2023-05-10 20:22:00 -0300

    Implementing DocStrings and Type Hinting, to refactored file_handler code

--- ClockInGenerator/file_handler.py
+++ ClockInGenerator/file_handler.py
@@ -1,21 +1,65 @@
 import json
 from collections import OrderedDict
 
-class JSONFileHandler:
-    def __init__(self, file_path):
+
+class JSONFileManager:
+    """Handles reading and writing data to a JSON file."""
+
+    def __init__(self, file_path: str):
+        """
+        Initialize JSONFileManager.
+
+        Args:
+            file_path (str): The path to the JSON file.
+        """
         self.file_path = file_path
 
-    def load_data(self):
+    def load_data(self) -> dict:
+        """
+        Load data from the JSON file.
+
+        Returns:
+            dict: The loaded JSON data.
+        """
         with open(self.file_path, 'r') as file:
             data = json.load(file)
         return data
 
-    def write_data(self, data):
+    def write_data(self, data: dict) -> None:
+        """
+        Write data to the JSON file.
+
+        Args:
+            data (dict): The data to be written to the JSON file.
+        """
+        with open(self.file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+
+class JSONDataProcessor:
+    """Handles data manipulation and updates in the JSON file."""
+
+    def __init__(self, file_manager: JSONFileManager):
+        """
+        Initialize JSONDataProcessor.
+
+        Args:
+            file_manager (JSONFileManager): The instance of JSONFileManager to handle file operations.
+        """
+        self.file_manager = file_manager
+
+    def add_data(self, new_data: dict) -> None:
+        """
+        Add new data to the JSON file.
+
+        Args:
+            new_data (dict): The new data to be added to the JSON file.
+        """
         # Load existing data
-        existing_data = self.load_data()
+        existing_data = self.file_manager.load_data()
 
         # Add new data to existing data
-        existing_data.update(data)
+        existing_data.update(new_data)
 
         # Sort the data by date in descending order
         sorted_data = OrderedDict(
@@ -26,5 +70,4 @@ class JSONFileHandler:
         latest_data = dict(list(sorted_data.items())[:30])
 
         # Write the latest data to the file
-        with open(self.file_path, 'w') as file:
-            json.dump(latest_data, file, indent=4)
+        self.file_manager.write_data(latest_data)
